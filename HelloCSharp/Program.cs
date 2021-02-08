0a672699e47723f111e379a12f2050eaf1ec4b2f
Morley Tatro <morleytatro@gmail.com>
2021-02-08 14:58:12 -0800

    first project

---
+++ HelloCSharp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System; // like an import statement
+using System.Collections.Generic;
+
+namespace HelloCsharp
+{
+    class Program
+    {
+        // this is the entrypoint to the app
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+
+            // Math myMathInstance = new Math();
+            int result = Math.Add(1, 2);
+
+            string myName = "Morley Tatro";
+
+            // string interpolation
+            Console.WriteLine($"My name is {myName}");
+
+            // int myInt = 5;
+
+            // don't do this!
+            // myInt = "some string";
+
+            int[] myArray = { 1, 2, 3, 4 };
+
+            myArray[2] = 5;
+            // myArray[4] = 100;
+
+            foreach(int item in myArray)
+            {
+                Console.WriteLine($"The value is {item}");
+            }
+
+            List<string> students = new List<string>();
+
+            students.Add("Nick");
+            students.Add("CJ");
+            students.Add("Tino");
+
+            students.Remove("CJ");
+
+            foreach(string student in students)
+            {
+                Console.WriteLine($"The student's name is {student}");
+            }
+
+            Console.WriteLine($"There are {students.Count} students!");
+
+            Dictionary<string, object> myDictionary = new Dictionary<string, object>();
+
+            myDictionary.Add("Age", 25);
+            myDictionary.Add("Name", "Morley");
+        }
+
+    }
+
+    class Math
+    {
+        // static keyword allows us to call the function directly from the class
+        // typically for utility functions
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ HelloCSharp/python_equivalents.py
@@ -0,0 +1,32 @@
+print("Hello World!")
+
+def some_func():
+  return "Hello"
+
+my_name = "Morley Tatro"
+
+print(f"My name is {my_name}")
+
+my_bool = True
+
+if my_age > 50:
+  print("I'm old!")
+elif my_age > 30:
+  print("I'm seasoned!")
+
+my_array = [
+  4,
+  5,
+  6
+]
+
+for val in my_array:
+  print(val)
+
+my_array.append(100)
+
+print(len(my_array))
+
+my_dictionary = {
+  "some_key": "some val"
+}
