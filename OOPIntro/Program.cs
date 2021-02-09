c77528b068f2c2e744faad7006c19a7245a9c659
Morley Tatro <morleytatro@gmail.com>
2021-02-09 15:44:15 -0800

    added another property to demonstrate getters and setters

--- OOPIntro/Program.cs
+++ OOPIntro/Program.cs
@@ -9,9 +9,10 @@ namespace OOPIntro
         {
             Student kyle = new Student("Kyle", "Marymee");
             Student ian = new Student("Ian", "Rones", 100);
-            Console.WriteLine(kyle.firstName);
-            // ian.energy = 40;
-
+            // Console.WriteLine(kyle.firstName);
+            Console.WriteLine(ian.FullName);
+            ian.MyPrivateProperty = "some new string!";
+            Console.WriteLine(ian.MyPrivateProperty);
             // Console.WriteLine(kyle);
             List<Student> students = new List<Student>() {
                 kyle,
@@ -31,7 +32,29 @@ namespace OOPIntro
         // these are fields
         public string firstName;
         public string lastName;
-        public int energy;
+        public int Energy {get;set;}
+
+        private string myPrivateField;
+
+        public string MyPrivateProperty {
+            get
+            {
+                return myPrivateField;
+            }
+            // just like a regular function that takes a value
+            set
+            {
+                if(value.Length < 50)
+                {
+                    Console.WriteLine("Sorry, that's too short!");
+                }
+                else
+                {
+                    myPrivateField = value;
+                }
+
+            }
+        }
 
         // example of a property
         public string FullName {
@@ -49,13 +72,15 @@ namespace OOPIntro
             // this runs every time we create a student!
             firstName = fName;
             lastName = lName;
-            energy = en;
+            Energy = en;
+
+            myPrivateField = "some string here!";
         }
 
         // optional override so we can see useful info
         public override string ToString()
         {
-            return $"{FullName} has {energy} energy.";
+            return $"{FullName} has {Energy} energy.";
         }
     }
 }
